2d260fabcf7148a1fb05d3fdf7ee24e8fc4f6cbb
Edu <ejramire@uc.cl>
2016-10-05 20:17:02 -0300

    dynamodb

--- tempo_analysis/app.py
+++ tempo_analysis/app.py
@@ -1,31 +1,72 @@
 from chalice import Chalice
+import spotipy
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 app = Chalice(app_name='tempo_analysis')
+app.debug = True
+spotify = spotipy.Spotify()
+dynamodb = boto3.resource('dynamodb')
+
+def refresh_token():
+    pass
+
+def load_from_db(artist_id):
+    table = dynamodb.Table('CheckedArtists')
+    db_results = table.query(
+        KeyConditionExpression=Key('artist_id').eq(artist_id)
+        )
+    if db_results['Count'] == 0:
+        return False
+    else:
+        table = dynamodb.Table('Tracks')
+        db_results = table.query(
+            KeyConditionExpression=Key('artist_id').eq(artist_id)
+            )
+        return db_results['Items']
+
+def parse_tracks(tracks):
+    response = []
+    for track in tracks:
+        response.append({
+            'artist_id': track['artists'][0]['id'],
+            'track_id': track['id'],
+            'track_name': track['name'],
+            'artist_name': track['artists'][0]['name'],
+            'duration': track['duration_ms']
+            })
+    return response
+
+def load_from_spotify(artist_id):
+    artist_uri = 'spotify:artist:' + artist_id
+    results = spotify.artist_albums(artist_uri)
+    albums = results['items']
+    response = []
+    while results['next']:
+        results = spotify.next(results)
+        albums.extend(results['items'])
+    for album in albums:
+        results = spotify.album_tracks(album['id'])
+        tracks = results['items']
+        while results['next']:
+            results = spotify.next(results)
+            tracks.extend(results['items'])
+    response = parse_tracks(tracks)
+    return response
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+def save(tracks):
+    table = dynamodb.Table('Tracks')
+    with table.batch_writer() as batch:
+        for track in tracks:
+            batch.put_item(Item=track)
+    #tableputitemchecked
 
+@app.route('/artist/{artist_id}')
+def index(artist_id):
+    tracks = load_from_db(artist_id)
+    if not tracks:
+        tracks = load_from_spotify(artist_id)
+        save(tracks)
+    return {'tracks': tracks}
 
-# The view function above will return {"hello": "world"}
-# whenver you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.json_body
-#     # Suppose we had some 'db' object that we used to
-#     # read/write from our database.
-#     # user_id = db.create_user(user_as_json)
-#     return {'user_id': user_id}
-#
-# See the README documentation for more examples.
-#
